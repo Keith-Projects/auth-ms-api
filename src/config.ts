2bdc71899c0217325804c9cea9bd48fc8f97d21d
KeithLocalHome <kblackwelder08@gmail.com>
2022-03-26 14:53:27 -0400

    old edit

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,23 @@
-
+const logLevel = `all`;
+export const config = {
+    port: 5000,
+    log4js: {
+        appenders: {
+            aws: {
+                type: "log4js-cloudwatch-appender",
+                region: 'us-east-1',
+                logGroup: `auth-ms-api`,
+                logStream: `${process.env.environment || 'local_dev'}`,
+            },
+            out: {
+                type: 'stdout'
+            }
+        },
+        categories: {
+            default: {
+                appenders: ['out', 'aws'],
+                level: `${logLevel}`
+            },
+        }
+    }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
+import Express from 'express';
+import { config } from './config';
+import * as aws from 'aws-sdk';
+import *  as log4js from 'log4js';
 
+log4js.configure(config.log4js);
+
+const app = Express();
+const port = config.port;
+
+export const logger = log4js.getLogger('[Server]');
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
